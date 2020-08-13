4a18d5ec455077ec3e886ba1b7b1242da9e49031
Andrew Tanuwijaya <andrew.tanuwijaya7dev@gmail.com>
2020-08-14 03:31:15 +0700

    add guest validation image sidebar
    add guest validation image sidebar

--- FinalProject/resources/views/adminlte/layout/sidebar.blade.php
+++ FinalProject/resources/views/adminlte/layout/sidebar.blade.php
@@ -14,7 +14,16 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset('assets/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+          @guest
+          
+          @if (Route::has('register'))
+            <img src="{{ asset('assets/img/guest.png') }}" class="img-circle elevation-2" alt="User Image">
+          @endif
+
+          @else
+          <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+
+          @endguest
         </div>
         <div class="info">
           <a href="#" class="d-block">{{ Auth::user()->name ?? 'Guest' }}</a>
