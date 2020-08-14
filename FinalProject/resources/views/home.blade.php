4cb873c7357970b0333ba544cb7d2a26c5d2ddf6
Andrew Tanuwijaya <andrew.tanuwijaya7dev@gmail.com>
2020-08-14 18:40:11 +0700

    add navbar home page at login
    add navbar home page at login

--- FinalProject/resources/views/home.blade.php
+++ FinalProject/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header" style="color: #FFFFFF; background-color: #28a745;">Success!</div>
 
                 <div class="card-body">
                     @if (session('status'))
--- FinalProject/resources/views/layouts/app.blade.php
+++ FinalProject/resources/views/layouts/app.blade.php
@@ -35,7 +35,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                        </li>
+                        <li class="nav-item d-none d-sm-inline-block">
+                            <a href="/" class="nav-link">Home</a>
+                        </li>
+                        <li class="nav-item d-none d-sm-inline-block">
+                            <a href="/question" class="nav-link">Questions</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
